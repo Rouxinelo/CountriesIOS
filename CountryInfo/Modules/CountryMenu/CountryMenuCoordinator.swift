a42ad03a8226e902edca740fafa5b7d64e83f39b
João Rouxinol <m54076@alunos.uevora.pt>
2023-03-17 22:26:33 +0000

    added navigation to the country menu

---
+++ CountryInfo/Modules/CountryMenu/CountryMenuCoordinator.swift
@@ -0,0 +1,17 @@
+//
+//  CountryMenuCoordinator.swift
+//  CountryInfo
+//
+//  Created by João Rouxinol on 17/03/2023.
+//
+
+import Foundation
+import UIKit
+
+class CountryMenuCoordinator: ModuleCoordinator {
+    func start() -> UIViewController {
+        let vc = CountryMenuViewController()
+        
+        return vc
+    }
+}
