6267ad24c288ad2c5cac8964bd09a57a5ed644c1
João Rouxinol <m54076@alunos.uevora.pt>
2023-03-24 22:24:30 +0000

    added header that expands and collapses

--- CountryInfo/Modules/CountryMenu/View/CountryMenuViewController.swift
+++ CountryInfo/Modules/CountryMenu/View/CountryMenuViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class CountryMenuViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    
     @IBOutlet var backgroundView: UIView!
     @IBOutlet weak var countryTableView: UITableView!
     @IBOutlet weak var countrySearchBar: UISearchBar!
+    
+    // MARK: - Functions
+    var hiddenSections = Set<String>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -57,7 +62,7 @@ extension CountryMenuViewController: UITableViewDataSource, UITableViewDelegate 
         countryTableView.register(UINib(nibName: "CountryCell", bundle: nil), forCellReuseIdentifier: "CountryCell")
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return hiddenSections.contains(String(section)) ? 0 : 3
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -67,5 +72,39 @@ extension CountryMenuViewController: UITableViewDataSource, UITableViewDelegate 
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return UITableView.automaticDimension
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+
+    
+    // MARK: - Headers logic
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        let sectionButton = UIButton()
+        sectionButton.setTitle(String(section), for: .normal)
+        sectionButton.backgroundColor = .white
+        sectionButton.tag = section
+        sectionButton.layer.borderColor = UIColor.black.cgColor
+        sectionButton.layer.borderWidth = 2
+        sectionButton.setTitleColor(.black, for: .normal)
+        sectionButton.addTarget(self,
+                                action: #selector(self.hideSection(sender:)),
+                                for: .touchUpInside)
+        
+        return sectionButton
+    }
+    
+    @objc func hideSection(sender: UIButton) {
+        guard let title = sender.titleLabel?.text else { return }
+        
+        if hiddenSections.contains(title) {
+            hiddenSections.remove(title)
+        } else {
+            hiddenSections.insert(title)
+        }
+        countryTableView.reloadData()
     }
 }
