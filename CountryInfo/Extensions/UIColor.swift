58c4d60f3f5ac710eaf20caf739c4c1f084ba960
João Rouxinol <m54076@alunos.uevora.pt>
2023-03-24 20:46:01 +0000

    added border color

--- CountryInfo/Extensions/UIColor.swift
+++ CountryInfo/Extensions/UIColor.swift
@@ -12,4 +12,5 @@ extension UIColor {
     static let lightSeaGreen = UIColor(red: 131/255, green: 197/255, blue: 190/255, alpha: 1)
     static let darkSeaGreen = UIColor(red: 0/255, green: 109/255, blue: 119/255, alpha: 1)
     static let backgroundWhite = UIColor(red: 237/255, green: 246/255, blue: 249/255, alpha: 1)
+    static let borderGray = UIColor(red: 108/255, green: 117/255, blue: 125/255, alpha: 1)
 }
